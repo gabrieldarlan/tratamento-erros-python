bac3da6b192d705676006f16164dfb576467e072
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-11-03 20:21:59 -0300

    cap02 completo

--- example.py
+++ example.py
@@ -1,19 +1,31 @@
 def dividir(dividendo, divisor):
-    print(dividir.resultado)
-    return dividendo / divisor
+    if not (isinstance(dividendo, int) and isinstance(divisor, int)):
+        raise ValueError("dividir() deve receber apenas argumentos inteiros")
+    try:
+        aux = dividendo / divisor
+    except:
+        print(f"Não foi possível dividir {dividendo} por {divisor}")
+        raise
+    return aux
 
 
 def testa_divisao(divisor):
-    try:
-        resultado = dividir(10, divisor)
-        print(f"O resultado da divisao de 10 por {divisor} é {resultado}")
-    except ZeroDivisionError:
-        print("Erro de divisão por zero tratado")
+    resultado = dividir(10, divisor)
+    print(f"O resultado da divisao de 10 por {divisor} é {resultado}")
 
+
+# try:
+#     testa_divisao(2.5)
+# except ZeroDivisionError as E:
+#     print("Erro de divisão por zero")
+# # except Exception as E:
+# #     print("Tratamento generico")
 
 try:
-    testa_divisao(0)
-except AttributeError:
-    print("Erro de atributo")
+    print("o fluxo esta aqui")
+    raise ValueError
+except Exception:
+    print("Agora ele foi pra cá")
 
+print("E enfim ele continua...")
 print("Programa encerrado")
